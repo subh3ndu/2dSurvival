6a4a6ab6d62f9d77b17ba964731cd58bb48d88de
Subhendu  Karmakar <subhendukarmakar61@gmail.com>
2024-06-25 17:03:46 +0530

    minor quality of life improvements

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 const MAX_SPEED = 200
 const GRAVITY = 0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass
 
-# Called every time. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
@@ -15,9 +15,10 @@ func _process(delta):
 	
 	move_and_slide()
 
+
 # Returns the movement_vector from the input.
 func get_movement_vector():
-	var movement_vector = Vector2.ZERO
+	#var movement_vector = Vector2.ZERO
 	
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
